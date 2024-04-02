e1de9363a3b3cf03b5884232f0a8078992445623
Julian Todd <julian@goatchurch.org.uk>
2024-04-02 20:02:44 +0100

    remove all opus code and refactor the onevoip PTT code

--- addons/player-networking/RecordingFeature.gd
+++ addons/player-networking/RecordingFeature.gd
@@ -1,9 +1,12 @@
 extends HBoxContainer
 
-
+var Ddisablevoip = false
+var Dstorespeechfilename = "user://welcomespeech.dat"
+var welcomespeechfilename = "res://addons/player-networking/welcomespeech.dat"
 
 var micrecordingdata = null
 const max_recording_seconds = 5.0
+
 var recordingnumberC = 1
 var recordingeffect = null
 var capturingeffect = null
@@ -14,83 +17,60 @@ var voipinputcapture = null  # class VOIPInputCapture
 
 var packetgaps = [ ]
 var packettime = 0
-func _packet_ready(packet): #New packet from mic to send
-	voipcapturepackets.append(packet)
-	voipcapturesize += len(packet)
-	var Npackettime = Time.get_ticks_usec()
-	packetgaps.append(Npackettime - packettime)
-	packettime = Npackettime
+func voip_packet_ready(packet): #New packet from mic to send
+	if voipcapturepackets != null:
+		voipcapturepackets.append(packet)
+		voipcapturesize += len(packet)
+		var Npackettime = Time.get_ticks_usec()
+		packetgaps.append(Npackettime - packettime)
+		packettime = Npackettime
 	
-var D = 0
+
+var recordingstart_msticks = 0
+var currentlyrecording = false
+
+var voipcapturepacketsplayback = null
+var voipcapturepacketsplaybackIndex = 0
+var voipcapturepacketsplaybackstart_msticks = 0
+var voipcapturepacketsplayback_msduration = 1
+
 func _process(delta):
-	if voipinputcapture:
-		D += 1
-		if (D%10) == 0:
+	if currentlyrecording:
+		if voipinputcapture:
 			voipinputcapture.send_test_packets()
-	
-func _ready():
-	set_process(false)  
-	print("VOIPInputCapture exists: ", ClassDB.class_exists("VOIPInputCapture"))
-	var recordbus_idx = AudioServer.get_bus_index("Recorder")
-#	assert ($MicRecord/AudioStreamRecorder.bus == "Recorder")
-	assert ($MicRecord/AudioStreamRecorder.stream.is_class("AudioStreamMicrophone"))
-	assert (AudioServer.is_bus_mute(recordbus_idx) == true)
-	recordingeffect = AudioServer.get_bus_effect(recordbus_idx, 0)
-	assert (recordingeffect.is_class("AudioEffectRecord"))
-
-	var mic_bus = AudioServer.get_bus_index("Mic")
-	if mic_bus != -1:
-		voipinputcapture = AudioServer.get_bus_effect(mic_bus, 0)
-		assert (voipinputcapture.is_class("VOIPInputCapture"))
-		voipinputcapture.packet_ready.connect(self._packet_ready)
-
-	# we can use this capturing object ring buffer to collect and batch up chunks
-	# see godot-voip demo.  Also how to use AudioStreamGeneratorPlayback etc
-	#capturingeffect = AudioServer.get_bus_effect(recordbus_idx, 1)
-	#assert (capturingeffect.is_class("AudioEffectCapture"))
-	var enablesound = true
-	if enablesound and ClassDB.class_exists("OpusEncoderNode"):
-		if enablesound:
-			var OpusEncoder = ClassDB.instantiate("OpusEncoderNode")
-			OpusEncoder.name = "OpusEncoder"
-			$MicRecord.add_child(OpusEncoder)
 		else:
-			print("Missing Opus plugin library")
-		if enablesound and ClassDB.class_exists("OpusDecoderNode"):
-			var OpusDecoder = ClassDB.instantiate("OpusDecoderNode")
-			OpusDecoder.name = "OpusDecoder"
-			$MicRecord.add_child(OpusDecoder)
+			pass # assert (recordingeffect.is_recording_active())
+		if (Time.get_ticks_msec() - recordingstart_msticks)/1000 > max_recording_seconds:
+			stop_recording()
 
-	if $MicRecord.has_node("OpusDecoder"):
-		var fname = "res://addons/player-networking/welcomespeech.opusbin"
-		if FileAccess.file_exists(fname):
-			micrecordingdata = { "format":1, "mix_rate":44100, "is_stereo":true }
-			micrecordingdata["opusEncoded"] = FileAccess.get_file_as_bytes(fname)
-			$RecordSize.text = "w-"+str(len(micrecordingdata.get("opusEncoded", micrecordingdata.get("pcmData"))))
+	if voipcapturepacketsplayback != null:
+		var currentplaybackduration = Time.get_ticks_msec() - voipcapturepacketsplaybackstart_msticks
+		var packetproportion = currentplaybackduration / voipcapturepacketsplayback_msduration
+		var targetvoipcapturepacketsplaybackIndex = packetproportion*len(voipcapturepacketsplayback) + 20
+		while voipcapturepacketsplaybackIndex < targetvoipcapturepacketsplaybackIndex and voipcapturepacketsplaybackIndex < len(voipcapturepacketsplayback):
+			$PlayRecord/AudioStreamPlayer.stream.push_packet(voipcapturepacketsplayback[voipcapturepacketsplaybackIndex])
+			voipcapturepacketsplaybackIndex += 1
 
-var recordingstart = 0
-func _on_MicRecord_button_down():
-	if not recordingeffect.is_recording_active():
-		recordingnumberC += 1
-		micrecordingdata = null
-		recordingstart = Time.get_ticks_msec()
 		
-		print("OS.get_name() ", OS.get_name())
-		if voipinputcapture != null:
-			voipcapturepackets = [ ]
-			voipcapturesize = 0
-			packetgaps = [ ]
-			set_process(true)
+		if currentplaybackduration > voipcapturepacketsplayback_msduration + 300:
+			voipcapturepacketsplayback = null
+			$PlayRecord/AudioStreamPlayer.stop()
+			$PlayRecord/AudioStreamPlayer.stream = null
 
-		await get_tree().create_timer(0.1).timeout
-		var lrecordingnumberC = recordingnumberC
-		recordingeffect.set_recording_active(true)
-		await get_tree().create_timer(max_recording_seconds).timeout
-		if micrecordingdata != null and lrecordingnumberC == recordingnumberC:
-			_on_MicRecord_button_up()
-		
-			
-func _on_MicRecord_button_up():
+func start_recording():
+	currentlyrecording = true
+	recordingstart_msticks = Time.get_ticks_msec()
+	if $VoipMode.button_pressed and voipinputcapture:
+		voipcapturepackets = [ ]
+		voipcapturesize = 0
+		packetgaps = [ ]
+	else:
+		recordingeffect.set_recording_active(true)  # begins storing the bytes from a recording
+
+func stop_recording():
+	currentlyrecording = false
+	var recording_duration = (Time.get_ticks_msec() - recordingstart_msticks)/1000.0 + 0.01
+	var underlyingbytessize = 0
 	if recordingeffect.is_recording_active():
 		recordingeffect.set_recording_active(false)
 		var recording = recordingeffect.get_recording()
@@ -98,69 +78,93 @@ func _on_MicRecord_button_up():
 		micrecordingdata = { "format":recording.get_format(), 
 							"mix_rate":recording.get_mix_rate(),
 							"is_stereo":recording.is_stereo(),
-							"duration_ms":Time.get_ticks_msec() - recordingstart }
-		if voipinputcapture != null:
-			micrecordingdata["voipcapturepackets"] = voipcapturepackets
-			set_process(false)
-			print("voipcapturesize ", voipcapturesize, " avg size ", voipcapturesize/len(voipcapturepackets))
-		elif $MicRecord.has_node("OpusEncoder"):
-			micrecordingdata["opusEncoded"] = $MicRecord/OpusEncoder.encode(pcmData)
-		else:
-			micrecordingdata["pcmData"] = pcmData
-		print(" created data bytes ", len(var_to_bytes(micrecordingdata)), "  ", len(pcmData))
-		$RecordSize.text = "l-"+str(len(micrecordingdata.get("opusEncoded", micrecordingdata.get("voipcapturepackets", micrecordingdata.get("pcmData")))))
+							"duration":recording_duration, 
+							"pcmData":pcmData }
+		underlyingbytessize = len(pcmData)
+	elif voipinputcapture:
+		micrecordingdata = { "voipcapturepackets":voipcapturepackets, 
+							 "duration":recording_duration }
+		underlyingbytessize = voipcapturesize
+		voipcapturepackets = null
+	print("created data bytes ", len(var_to_bytes(micrecordingdata)), " underlying ", underlyingbytessize)
+	$RecordSize.text = "l-"+str(underlyingbytessize)
+
+func _ready():
+	if Ddisablevoip:
+		set_process(false)  
+		$MicRecord.disabled = true
+		$MicRecord/AudioStreamRecorder.playing = false
+		return
+		
+	var fin = FileAccess.open(welcomespeechfilename, FileAccess.READ)
+	if fin:
+		micrecordingdata = fin.get_var()
+		fin.close()
+
+	var recordbus_idx = AudioServer.get_bus_index("Recorder")
+	assert ($MicRecord/AudioStreamRecorder.stream.is_class("AudioStreamMicrophone"))
+	assert ($MicRecord/AudioStreamRecorder.bus == "Recorder")
+	assert (AudioServer.is_bus_mute(recordbus_idx) == true)
+	recordingeffect = AudioServer.get_bus_effect(recordbus_idx, 0)
+	assert (recordingeffect.is_class("AudioEffectRecord"))
+
+	# upgrade to OneVoip system if addon from https://github.com/RevoluPowered/one-voip-godot-4/ detected
+	# The VOIPInputCapture taps off the stream leaving it the same, but feeds it to the Recorder
+	if ClassDB.class_exists("VOIPInputCapture"):
+		var voipbusidx = AudioServer.get_bus_count()
+		AudioServer.add_bus()
+		assert (AudioServer.get_bus_name(voipbusidx).begins_with("New Bus"))
+		AudioServer.set_bus_name(voipbusidx, "voiprecorder")
+		voipinputcapture = VOIPInputCapture.new()
+		voipinputcapture.set_buffer_length(0.5)   # In the inhereted AudioEffectCapture 
+		AudioServer.add_bus_effect(voipbusidx, voipinputcapture)
+		AudioServer.set_bus_send(voipbusidx, "Recorder")
+		$MicRecord/AudioStreamRecorder.bus = "voiprecorder"
+		print($MicRecord/AudioStreamRecorder.bus)
+		voipinputcapture.packet_ready.connect(voip_packet_ready)
+		$VoipMode.button_pressed = true
+	else:
+		$VoipMode.disabled = true
 
 
+func _on_MicRecord_button_down():
+	if not currentlyrecording:
+		start_recording()
+
+func _on_MicRecord_button_up():
+	if currentlyrecording:
+		stop_recording()
 
 @rpc("any_peer") func remotesetmicrecord(lmicrecordingdata):
 	micrecordingdata = lmicrecordingdata
-	$RecordSize.text = "r-"+str(len(micrecordingdata.get("opusEncoded", micrecordingdata.get("pcmData"))))
-	
+	$RecordSize.text = "r-"+str(len(micrecordingdata.get("voipcapturepackets", micrecordingdata.get("pcmData"))))
 
 func _on_SendRecord_pressed():
 	if micrecordingdata != null:
 		rpc("remotesetmicrecord", micrecordingdata)
-	#var fout = File.new()
-	#var fname = "user://welcomespeech.dat"
-	#print("saving ", ProjectSettings.globalize_path(fname))
-	#fout.open(fname, File.WRITE)
-	#fout.store_var(micrecordingdata)
-	#fout.close()
+	if Dstorespeechfilename:
+		print("saving ", ProjectSettings.globalize_path(Dstorespeechfilename))
+		var fout = FileAccess.open(Dstorespeechfilename, FileAccess.WRITE)
+		fout.store_var(micrecordingdata)
+		fout.close()
 
 func _on_PlayRecord_pressed():
 	print("_on_PlayRecord_pressed")
-	if micrecordingdata != null:
-		var audioStream = AudioStreamWAV.new()
+	var audioStream = null
+	if micrecordingdata != null and micrecordingdata.has("pcmData"):
+		audioStream = AudioStreamWAV.new()
 		audioStream.set_format(micrecordingdata["format"])
-		audioStream.set_mix_rate(micrecordingdata["mix_rate"])
+		audioStream.set_mix_rate(micrecordingdata["mix_rate"])		
 		audioStream.set_stereo(micrecordingdata["is_stereo"])
-		var voipcapturepackets = [ ]
-		if micrecordingdata.has("voipcapturepackets") and ClassDB.class_exists("AudioStreamVOIP"):
-			audioStream = ClassDB.instantiate("AudioStreamVOIP")
-			voipcapturepackets = micrecordingdata["voipcapturepackets"]
-			print("num packets ", len(voipcapturepackets), " in duration ", micrecordingdata["duration_ms"])
-			print("milliseconds per packet ", micrecordingdata["duration_ms"]/len(voipcapturepackets))
-			print("audio playing voip ")
-		elif micrecordingdata.has("opusEncoded") and $MicRecord.has_node("OpusDecoder"):
-			audioStream.data = $MicRecord/OpusDecoder.decode(micrecordingdata["opusEncoded"])
-			print("audio playing bytes ", len(audioStream.data))
-		elif micrecordingdata.has("pcmData"):
-			audioStream.data = micrecordingdata["pcmData"]
-			print("audio playing bytes ", len(audioStream.data))
-		else:
-			print("No decodable audio data here")
+		audioStream.data = micrecordingdata["pcmData"]
+	elif micrecordingdata != null and micrecordingdata.has("voipcapturepackets") and ClassDB.class_exists("AudioStreamVOIP"):
+		audioStream = ClassDB.instantiate("AudioStreamVOIP")
+		voipcapturepacketsplayback = micrecordingdata["voipcapturepackets"]
+		voipcapturepacketsplaybackIndex = 0
+		voipcapturepacketsplaybackstart_msticks = Time.get_ticks_msec()
+		voipcapturepacketsplayback_msduration = micrecordingdata["duration"]*1000
+	else:
+		print("Can't deal with micrecordingdata ", micrecordingdata.keys())
+	if audioStream != null:
 		$PlayRecord/AudioStreamPlayer.stream = audioStream
 		$PlayRecord/AudioStreamPlayer.play()
-
-		if voipcapturepackets:
-			print("num packets ", len(voipcapturepackets), " in duration ", micrecordingdata["duration_ms"])
-			var playstart = Time.get_ticks_msec()
-			for i in range(len(voipcapturepackets)):
-				audioStream.push_packet(voipcapturepackets[i])
-				if (i%5) == 1:
-					await get_tree().process_frame
-			var pushduration = Time.get_ticks_msec() - playstart
-			await get_tree().create_timer((micrecordingdata["duration_ms"] - pushduration)/1000).timeout
-			await get_tree().create_timer(0.1).timeout
-			$PlayRecord/AudioStreamPlayer.stop()
-
