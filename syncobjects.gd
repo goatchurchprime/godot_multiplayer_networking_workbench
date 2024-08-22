8f499aae183d9739479e3736913c395d83f5e8fd
Julian Todd <julian@goatchurch.org.uk>
2024-08-22 19:29:16 +0100

    major making of the synchronizer spawn functions and taking control of the authority

--- syncobjects.gd
+++ syncobjects.gd
@@ -3,20 +3,72 @@ extends Node2D
 @rpc("any_peer", "call_local")
 func ssss(me):
 	print("ssss ", me, "  ll ", multiplayer.get_unique_id(), " ", $RigidBody2D.get_multiplayer_authority(), " :", multiplayer.get_remote_sender_id())
+
+var spawntoyscene = load("res://mulitplayerspawntoy.tscn")
 	
 # The MultiplayerSynchronizer synchronizes in the direction of the authority to the peers
 func _ready():
 	$MultiplayerSynchronizer.rpc_config("set_multiplayer_authority", {"call_local":true, "rpc_mode":MultiplayerAPI.RPC_MODE_ANY_PEER})
-	$spawnedtoy.mouse_entered.connect(mouseenter.bind("thinkg"))
 	Input.set_default_cursor_shape(Input.CURSOR_CROSS)
+	$MultiplayerSpawner.set_spawn_function(spawnthistoy)
+	spawnnexttoy(Vector2i(300, 700))
 
-func Dmouseenter():
-	print("sdfs")
-func mouseenter(eekl):
-	print("dasda ", eekl)
-	
+var currentmousetoy = null
+var relmouse = null
+var topzindex = 2
+func mouseenter(mousetoy):
+	print("ment ", mousetoy)
+	if relmouse == null: 
+			# need a more complex system keeping track of all the ins
+			# and what's highest
+			# or doing a ray collision check when we click 
+		if currentmousetoy != null:
+			currentmousetoy.get_node("MouseIn").visible = false
+		currentmousetoy = mousetoy
+		currentmousetoy.get_node("MouseIn").visible = true
+
+func mouseexit(mousetoy):
+	print(" mexit ", mousetoy)
+	mousetoy.get_node("MouseIn").visible = false
+	if currentmousetoy == mousetoy:
+		if relmouse == null: 
+			currentmousetoy.get_node("MouseIn").visible = false
+			currentmousetoy = null
+
+func interactcurrenttoy(pressed):
+	if pressed and relmouse == null and currentmousetoy != null:
+		relmouse = get_global_mouse_position() - currentmousetoy.get_global_position()
+		if currentmousetoy.z_index < topzindex:
+			topzindex += 1
+			currentmousetoy.z_index = topzindex
+		if currentmousetoy.get_node("MultiplayerSynchronizer").get_multiplayer_authority() != multiplayer.get_unique_id():
+			currentmousetoy.get_node("MultiplayerSynchronizer").rpc("set_multiplayer_authority", multiplayer.get_unique_id())
+
+
+	elif relmouse != null and not pressed:
+		relmouse = null
+
+func motioncurrenttoy(gpos):
+	if currentmousetoy != null and relmouse != null:
+		currentmousetoy.global_position = gpos - relmouse
+
+func spawnthistoy(data):
+	print("spawnthistoy ", data)
+	var k = spawntoyscene.instantiate()
+	k.get_node("Label").text = data["letter"]
+	k.get_node("MultiplayerSynchronizer").rpc_config("set_multiplayer_authority", {"call_local":true, "rpc_mode":MultiplayerAPI.RPC_MODE_ANY_PEER})
+	return k
+
 var xx = 0
 var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+func spawnnexttoy(gpos):
+	var h = $MultiplayerSpawner.spawn({"letter":letters[xx]})
+	xx += 1
+	h.global_position = gpos
+#	h.mouse_entered.connect(mouseenter.bind(h))
+	h.mouse_exited.connect(mouseexit.bind(h))
+	return
+
 func _input(event):
 	if event is InputEventKey and event.pressed and event.keycode == KEY_U:
 		prints($spawnedtoy.get_multiplayer_authority(), multiplayer.get_unique_id())
@@ -33,27 +85,11 @@ func _input(event):
 		$MultiplayerSynchronizer.rpc("set_multiplayer_authority", multiplayer.get_unique_id())
 
 	if event is InputEventKey and event.pressed and event.keycode == KEY_Q:
-		xx += 1
-		var kk = load("res://mulitplayerspawntoy.tscn")
-		print(kk)
-		var k = kk.instantiate()
-		k.position.x = xx*30  
-		k.position.y = 150  
-		k.get_node("Label").text = letters[xx]
-		add_child(k, true)
-		print(k)
+		spawnnexttoy(get_global_mouse_position())
 
-#	if event is InputEventMouseMotion:
-#		if $spawnedtoy.relmouse == null:
-#			$spawnedtoy.global_position = event.global_position
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		interactcurrenttoy(event.pressed)
 		#get_viewport().set_input_as_handled()
-
-func _on_spawnedtoy_input_event(viewport, event, shape_idx):
-	pass
-	if event is not InputEventMouseMotion:
-		print(viewport, event, shape_idx)
-
-
-func _on_spawnedtoy_mouse_entered():
-	print("mmm:")
+	if event is InputEventMouseMotion:
+		motioncurrenttoy(event.global_position)
 	
